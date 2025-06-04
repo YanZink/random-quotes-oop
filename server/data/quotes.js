7559d9d768b06c29ea42b4d38b6670640d8937b9
YanZink <zinkovskii1803@gmail.com>
2025-06-04 10:51:07 +0300

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: "a1b2c3d4",
+    text: "The greatest wealth is to live content with little",
+    author: "Plato",
+  },
+  {
+    id: "e5f6g7h8",
+    text: "Knowing yourself is the beginning of all wisdom",
+    author: "Aristotle",
+  },
+  {
+    id: "i9j0k1l2",
+    text: "Happiness depends upon ourselves",
+    author: "Aristotle",
+  },
+  {
+    id: "m3n4o5p6",
+    text: "The only true wisdom is in knowing you know nothing",
+    author: "Socrates",
+  },
+  {
+    id: "q7r8s9t0",
+    text: "Be the change that you wish to see in the world",
+    author: "Mahatma Gandhi",
+  },
+  {
+    id: "u1v2w3x4",
+    text: "In the middle of difficulty lies opportunity",
+    author: "Albert Einstein",
+  },
+  {
+    id: "y5z6a7b8",
+    text: "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment",
+    author: "Buddha",
+  },
+  {
+    id: "c9d0e1f2",
+    text: "It does not matter how slowly you go as long as you do not stop",
+    author: "Confucius",
+  },
+  {
+    id: "g3h4i5j6",
+    text: "Quality is not an act, it is a habit",
+    author: "Aristotle",
+  },
+  {
+    id: "k7l8m9n0",
+    text: "The journey of a thousand miles begins with one step",
+    author: "Lao Tzu",
+  },
+];
+
+module.exports = quotes;
